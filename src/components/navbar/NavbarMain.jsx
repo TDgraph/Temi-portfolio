9a96d483bedcef0d378f861b23cb23712c6b2dfd
TDgraph <taymeedouglas07@gmail.com>
2025-05-29 19:32:26 +0100

    update

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -22,7 +22,7 @@ const NavbarMain = () => {
   return (
     <nav className="max-w-[1300px] mx-auto px-4 w-full fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2">
 
-      {/* Put our component inside a div */}
+      {/* creating a component for our navlogo */}
      <div className="flex justify-between w-full max-w-[1200px] mx-auto bg-black items-center p-6 rounded-r-full rounded-l-full border-[0.5px] border-cyan">
          <NavbarLogo />
 
